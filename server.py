4c2cd6d2b301f77ffcee5bbf4eb21fca56f60e08
Tony Man <tonyman316@gmail.com>
2018-10-04 22:57:16 -0700

    add hw1 skeleton

--- server.py
+++ server.py
@@ -1,32 +1,8 @@
 from bottle import get, route, run, template, view, static_file
 
-@get('/index')
-def index():
-    return "Welcome to CMPS 183!"
-
-@get('/greet/<name>')
-def greet(name):
-    return template('greet_template', name=name)
-
-@view('greet_template')
-@get('/sayhi/<name>')
-def greet(name):
-    return dict(name=name)
-
-# Our cat web page
-@get('/cats')
-def cats():
-    return template('cats_template')
-
-# Let's add some code to serve jpg images from our static images directory.
-@route('/images/<filename:re:.*\.jpg>')
-def serve_image(filename):
-    return static_file(filename, root='images', mimetype='image/jpg')
-
-# Code for serving css stylesheets from /css directory.
-@route('/css/<filename:re:.*.css>')
-def serve_css(filename):
-    return static_file(filename, root='css', mimetype='text/css')
+@get('/')
+def news():
+    return template("news_template")
 
 
 run(reloader=True, host='localhost', port=8080)
